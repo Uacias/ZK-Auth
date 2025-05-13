915b6bfe0919119430dcfa99025269ac8db9e458
Uacias <filgum0326@gmail.com>
2025-05-13 21:31:44 +0200

    Frontend integration

---
+++ frontend/src/routes/hash-login/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import LoginForm from '$lib/ui/LoginForm.svelte';
+</script>
+
+<div class="content">
+	<h2 class="text-primary mb-4 text-2xl font-bold">Hashed Login</h2>
+	<LoginForm endpoint="login_hashed" />
+</div>
